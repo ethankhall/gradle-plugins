0ee75bcc3c1851cb0f3a32327f150ad7bf5016ed
Ethan Hall <ethan@ehdev.io>
2017-05-07 21:05:29 -0700

    Making extension easier to debug

--- dependency-script/plugin/src/main/kotlin/io/ehdev/gradle/dependency/plugin/DependencyExtension.kt
+++ dependency-script/plugin/src/main/kotlin/io/ehdev/gradle/dependency/plugin/DependencyExtension.kt
@@ -1,3 +1,7 @@
 package io.ehdev.gradle.dependency.plugin
 
-open class DependencyExtension(val libraries: Map<String, String>, val versions: Map<String, String>)
+open class DependencyExtension(val libraries: Map<String, String>, val versions: Map<String, String>) {
+    override fun toString(): String {
+        return "DependencyExtension(libraries=$libraries, versions=$versions)"
+    }
+}
